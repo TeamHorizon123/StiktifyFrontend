1b06fba58dba8570e388fd013df5b9989ae14dc9
ChinhTC <assin2910@gmail.com>
2025-06-29 09:25:35 +0700

    fix: change route of product info

--- src/utils/roleRoute.ts
+++ src/utils/roleRoute.ts
@@ -25,7 +25,7 @@ const roleRoutes: Record<string, (string | RegExp)[]> = {
     "/page/order-confirmation",
     "/page/purchasehistory",
     "/page/sticktify-shop",
-    /^\/page\/product-detail\/[\w-]+$/,
+    /^\/page\/shop\/product\/[\w-]+$/,
     "/page/karaoke",
   ],
   GUEST: [
